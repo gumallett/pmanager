008d7f40d0c04efa4e0faef54f14b74200af085e
greg <gumallett@gmail.com>
2021-05-09 10:09:09 -0400

    initial commit

---
+++ src/main/kotlin/com/gum/pmanager/PmanagerApplication.kt
@@ -0,0 +1,11 @@
+package com.gum.pmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PmanagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<PmanagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/gum/pmanager/PmanagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gum.pmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PmanagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
