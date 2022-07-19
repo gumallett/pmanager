140bc68cac53fa37baeb12902bb9a4fe0a993af5
greg <gumallett@gmail.com>
2022-07-18 20:06:32 -0400

    Small fixes

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -2,6 +2,5 @@
 
 CUR_WD=`pwd`
 
-cd /mnt/e/ /letter/new11
-find . -name "*.mp4" -exec "$CUR_WD/./scripts/video_prep.sh" {} \;
-
+echo "Working dir: $1"
+find "$1" -name "*.mp4" -exec "$CUR_WD/./scripts/video_prep.sh" {} \;
